6baf39929d7a65fc43278ac545207fb046b413de
Eric Sage <eric.david.sage@gmail.com>
2016-09-10 10:08:45 +0000

    Add clear button to text box

--- src/filters/TextBox.jsx
+++ src/filters/TextBox.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import RaisedButton from 'material-ui/RaisedButton'
+
 export default class TextBox extends React.Component {
 
   static getIcon() { return "note" }
@@ -8,25 +10,41 @@ export default class TextBox extends React.Component {
     this.props.updateQuery(event.target.value)
   }
 
+  clear() {
+    this.props.updateQuery("")
+  }
+
   render() {
-    const style = {
+    const boxStyle={
+      height: '100%',
+      width: '100%',
+    }
+    const textStyle = {
       margin: '2%',
       marginTop: '5%',
       marginBottom: 0,
-      height: '80%',
-      width: '95%',
+      height: '90%',
       resize: 'none',
       border: 0,
       fontSize: 20,
       overflowY: 'auto'
     }
     return (
-        <textarea
-          style={style}
-          placeholder="Enter your search terms here..."
-          value={this.props.query}
-          onChange={this.handleChange.bind(this)}
-        />
+        <div style={boxStyle}>
+          <textarea
+            style={textStyle}
+            placeholder="Enter your search terms here..."
+            value={this.props.query}
+            onChange={this.handleChange.bind(this)}
+          />
+          <RaisedButton
+            style={{ margin: 0, padding: 0 }}
+            onClick={this.clear.bind(this)}
+            label="Clear"
+            primary={true}
+            fullWidth={true}
+          />
+        </div>
     )
   }
 
