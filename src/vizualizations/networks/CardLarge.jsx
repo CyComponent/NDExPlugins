7b1d8ed39f6240c38bec912815b8dae87196631a
Keiichiro Ono <keiono@gmail.com>
2016-09-20 18:42:35 -0700

    Large card design updated.

--- src/vizualizations/networks/CardLarge.jsx
+++ src/vizualizations/networks/CardLarge.jsx
@@ -1,6 +1,3 @@
-
-import DropDownMenu from 'material-ui/DropDownMenu'
-import MenuItem from 'material-ui/MenuItem'
 import React from 'react'
 
 import {
@@ -14,45 +11,99 @@ import {
 
 import RaisedButton from 'material-ui/RaisedButton'
 
+
 export default class CardLarge extends React.Component {
 
   static getIcon() { return "view_stream" }
+
 
   handleSelection(network) {
     this.props.addToCart(network.externalId, network)
   }
 
+
   render() {
-    const cardStackStyle = {
-      height: '95%'
+
+    const cardContainer = {
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100%',
+      overflowY: 'scroll',
+      overflowX: 'hidden'
     }
-    var networks = this.props.summaries.toArray()
+
+    const cardStyle = {
+      margin: '0',
+      marginRight: '1em',
+      marginTop: '0.7em',
+    }
+
+    const titleStyle = {
+      width: '100%',
+      fontWeight: '300',
+      wordWrap: 'break-word',
+    }
+
+    const actionStyle = {
+      width: '17%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+
+    }
+
+    const networks = this.props.summaries.toArray()
+
     return (
-     <div style = {cardStackStyle}>
-       <div style={{ overflow: 'scroll', height: '95%' }}>
+
+       <div style={cardContainer}>
+
          {networks.map(N => (
-            <Card style={{ margin: "20"}}>
-              <CardHeader
-                title={"Created on " + N.creationTime}
-                subtitle={"Modified on " + N.modificationTime}
-              />
-              <CardMedia>
+
+            <Card style={cardStyle}>
+
+              <CardMedia
+                overlayContentStyle={{backgroundColor: 'rgba(100, 100, 100, 0.7)'}}
+                overlay={
+
+                  <div style={{display: 'flex'}}>
+                    <div style={{width: '83%'}}>
+                      <CardTitle
+                        title={N.name}
+                        titleColor={'#FFFFFF'}
+                        titleStyle={titleStyle}
+                        subtitle={
+                          <div>
+                            Owner: {N.owner}, Created: {N.creationTime}, Modified: {N.modificationTime}
+                          </div>
+                        }
+                        subtitleColor={'#DDDDDD'}
+                      />
+                      <CardText
+                        color={'#FFFFFF'}
+                      >
+                        {N.description}
+                      </CardText>
+                    </div>
+
+                    <CardActions style={actionStyle}>
+                      <RaisedButton
+                        labelColor={'#666666'}
+                        label="Add"
+                        onClick={this.handleSelection.bind(this, N)}
+                      />
+                    </CardActions>
+                  </div>
+                }
+              >
                 <NetworkImage N={N}/>
               </CardMedia>
-              <CardTitle title={N.name} subtitle={N.owner}/>
-              <CardText>{N.description}</CardText>
-              <CardActions>
-                <RaisedButton
-                  label="Add to Cart"
-                  primary={true}
-                  onClick={this.handleSelection.bind(this, N)}
-                />
-              </CardActions>
+
             </Card>
+
             )
           )}
         </div>
-      </div>
     )
   }
 }
@@ -70,18 +121,39 @@ class NetworkImage extends React.Component {
   }
 
   render() {
-    var style = {}
+
     if (this.state.error) {
-      style = { display: 'none' }
+      const imgStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '40%',
+        paddingTop: '2em',
+        backgroundColor: '#DFDFDF',
+        color: '#DDDDDD',
+        fontSize: '4em'
+      }
+
+      return (
+        <div style={imgStyle}>
+          No Image
+        </div>
+      )
+    } else {
+      const imgStyle = {
+        width: '100%',
+        minWidth: 0,
+        margin: 0,
+        padding: 0
+      }
+
+      return (
+        <img
+          src={"http://ci-dev-serv.ucsd.edu/" + this.props.N.externalId + ".png"}
+          onError={this.error}
+          style={imgStyle}
+        />
+      )
     }
-    return (
-      <img
-        src={"http://ci-dev-serv.ucsd.edu/" + this.props.N.externalId + ".png"}
-        onError={this.error}
-        style={Object.assign({ width: '50%', minWidth: 0, display: 'block', margin: 'auto' }, style)}
-      />
-    )
-
   }
-
 }
